679b08e910dbccac927e73ae01e06cbdc9ca031b
rangh <rangh@rangh.workstation>
2018-04-11 20:37:48 +0800

    update blog code

--- blog.go
+++ blog.go
@@ -1,30 +1,68 @@
 package magicblog
 
 import (
+	"log"
 	"net/http"
 
 	engine "muidea.com/magicEngine"
 )
 
-type blog struct {
+type route struct {
+	pattern string
+	method  string
+	handler interface{}
 }
 
-func (s *blog) Startup(router engine.Router) {
+func (s *route) Pattern() string {
+	return s.pattern
+}
+
+func (s *route) Method() string {
+	return s.method
+}
+
+func (s *route) Handler() interface{} {
+	return s.handler
+}
+
+func newRoute(pattern, method string, handler interface{}) engine.Route {
+	return &route{pattern: pattern, method: method, handler: handler}
+}
+
+// NewBlog 新建Blog
+func NewBlog(centerServer, account, authToken string) Blog {
+	return Blog{}
+}
+
+// Blog Blog对象
+type Blog struct {
+}
+
+// Startup 启动
+func (s *Blog) Startup(router engine.Router) {
+	mainRoute := newRoute("/", "GET", s.mainPage)
+	router.AddRoute(mainRoute)
+
+	detailRoute := newRoute("/detail/:id", "GET", s.detailPage)
+	router.AddRoute(detailRoute)
+
+	catalogRoute := newRoute("/catalog/:id", "GET", s.catalogPage)
+	router.AddRoute(catalogRoute)
+}
+
+// Teardown 销毁
+func (s *Blog) Teardown() {
 
 }
 
-func (s *blog) Teardown() {
-
+func (s *Blog) mainPage(res http.ResponseWriter, req *http.Request) {
+	log.Print("mainPage")
 }
 
-func (s *blog) MainPage(res http.ResponseWriter, req *http.Request) {
-
+func (s *Blog) catalogPage(res http.ResponseWriter, req *http.Request) {
+	log.Print("catalogPage")
 }
 
-func (s *blog) CatalogPage(res http.ResponseWriter, req *http.Request) {
-
-}
-
-func (s *blog) DetailPage(res http.ResponseWriter, req *http.Request) {
-
+func (s *Blog) detailPage(res http.ResponseWriter, req *http.Request) {
+	log.Print("detailPage")
 }
--- cmd/magicBlog/main.go
+++ cmd/magicBlog/main.go
@@ -3,27 +3,32 @@ package main
 import (
 	"flag"
 
+	magicblog "muidea.com/magicBlog"
 	engine "muidea.com/magicEngine"
 )
 
 var serviceAddress = ":8010"
 var centerServer = "http://127.0.0.1:8080/api/remote"
+var account = "test"
+var authToken = "token"
 
 func main() {
 	flag.StringVar(&serviceAddress, "Address", serviceAddress, "blogService listen address")
 	flag.StringVar(&centerServer, "CenterSvr", centerServer, "magicCenter server")
+	flag.StringVar(&account, "Account", account, "magicCenter server")
+	flag.StringVar(&authToken, "AuthToken", authToken, "magicCenter server")
 	flag.Parse()
 
 	router := engine.NewRouter()
 
-	//test.Append(router)
+	blog := magicblog.NewBlog(centerServer, account, authToken)
+
+	blog.Startup(router)
 
 	svr := engine.NewHTTPServer(serviceAddress)
 	svr.Bind(router)
 
-	//svr.Use(&test.Hello{})
-
-	//svr.Use(&test.Test{})
-
 	svr.Run()
+
+	blog.Teardown()
 }

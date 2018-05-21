b92fdc42cb6b7b62646c99d022a483ede146d699
muidea <rangh@foxmail.com>
2018-05-21 21:05:45 +0800

    update ui code

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -43,17 +43,5 @@ module.exports = (webpackConfig, env) => {
     }),
   ])
 
-  // Alias
-  webpackConfig.resolve.alias = {
-    components: `${__dirname}/src/components`,
-    utils: `${__dirname}/src/utils`,
-    config: `${__dirname}/src/utils/config`,
-    enums: `${__dirname}/src/utils/enums`,
-    services: `${__dirname}/src/services`,
-    models: `${__dirname}/src/models`,
-    routes: `${__dirname}/src/routes`,
-    themes: `${__dirname}/src/themes`,
-  }
-
   return webpackConfig
 }

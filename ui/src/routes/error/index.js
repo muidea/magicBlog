9019c4339427e056fea23ee38702a3bf02e2149e
muidea <rangh@foxmail.com>
2018-05-24 17:09:30 +0800

    update code

--- ui/src/routes/error/index.js
+++ ui/src/routes/error/index.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'dva'
-import { Row, Col } from 'antd'
-import { RichView, EditableTagGroup } from '../../components'
+import { Row } from 'antd'
+import { RichView } from '../../components'
 import styles from './index.less'
 
 function ErrorPage({ error }) {
-  const { name, creater, createDate, catalog, content } = error
+  const { content } = error
 
   return (
     <div className={styles.normal}>
-      <Row gutter={24} type="flex" justify="center"><Col><h1>{name}</h1></Col></Row>
-      <Row gutter={24} type="flex" justify="center"><span>作者：{creater.name}</span> 分类：<EditableTagGroup readOnly value={catalog} /> <span>创建时间：{createDate}</span></Row>
       <Row gutter={24}><RichView value={content} /> </Row>
     </div>
   )

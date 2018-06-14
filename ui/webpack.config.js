520cc40111ed72808bd918722fc011b18d86d0c5
muidea <rangh@foxmail.com>
2018-06-14 23:34:53 +0800

    update code

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -43,5 +43,17 @@ module.exports = (webpackConfig, env) => {
     }),
   ])
 
+  // Alias
+  webpackConfig.resolve.alias = {
+    components: `${__dirname}/src/components`,
+    utils: `${__dirname}/src/utils`,
+    config: `${__dirname}/src/utils/config`,
+    enums: `${__dirname}/src/utils/enums`,
+    services: `${__dirname}/src/services`,
+    models: `${__dirname}/src/models`,
+    routes: `${__dirname}/src/routes`,
+    themes: `${__dirname}/src/themes`,
+  }
+
   return webpackConfig
 }

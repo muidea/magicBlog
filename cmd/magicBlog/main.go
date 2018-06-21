1e94fd247f65eb917ec29feb11132b3ad58b0d30
muidea <rangh@foxmail.com>
2018-06-21 19:44:06 +0800

    add Dockerfile

--- cmd/magicBlog/main.go
+++ cmd/magicBlog/main.go
@@ -8,14 +8,14 @@ import (
 	engine "muidea.com/magicEngine"
 )
 
-var serviceAddress = ":8866"
+var bindPort = "8866"
 var centerServer = "http://127.0.0.1:8888"
 var blogName = "magicBlog"
 var endpointID = "f0e078a8-6de8-4273-88a4-dccef60ff88f"
 var authToken = "yTtWiuuoGifPVfcK5Mf4mdu8mGl78E3y"
 
 func main() {
-	flag.StringVar(&serviceAddress, "Address", serviceAddress, "blogService listen address")
+	flag.StringVar(&bindPort, "ListenPort", bindPort, "blogService listen address")
 	flag.StringVar(&centerServer, "CenterSvr", centerServer, "magicCenter server")
 	flag.StringVar(&blogName, "BlogName", blogName, "blog name.")
 	flag.StringVar(&endpointID, "EndpointID", endpointID, "magicBlog endpoint id")
@@ -28,7 +28,7 @@ func main() {
 	if ok {
 		blog.Startup(router)
 
-		svr := engine.NewHTTPServer(serviceAddress)
+		svr := engine.NewHTTPServer(bindPort)
 		svr.Bind(router)
 
 		svr.Run()
---
+++ docker/bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+EXTRA_ARGS=$EXTRA_ARGS
+if [ $LISTENPORT ]; then
+    EXTRA_ARGS=$EXTRA_ARGS' -ListenPort='$LISTENPORT
+fi
+
+if [ $CENTER_SERVER ]; then
+    EXTRA_ARGS=$EXTRA_ARGS' -CenterSvr='$CENTER_SERVER
+fi
+
+if [ $NAME ]; then
+    EXTRA_ARGS=$EXTRA_ARGS' -BlogName='$NAME
+fi
+
+if [ $ENDPOINTID ]; then
+    EXTRA_ARGS=$EXTRA_ARGS' -EndpointID='$ENDPOINTID
+fi
+
+if [ $AUTHTOKEN ]; then
+    EXTRA_ARGS=$EXTRA_ARGS' -AuthToken='$AUTHTOKEN
+fi
+
+echo $EXTRA_ARGS
+
+/var/app/magicBlog $EXTRA_ARGS "$@"

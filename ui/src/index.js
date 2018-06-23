731ff0d983d554c66cca8b9a31f622433ab733b5
muidea <rangh@foxmail.com>
2018-06-23 16:37:31 +0800

    update nginx.conf

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,7 +1,7 @@
 import { message } from 'antd'
 import dva from 'dva'
 import createLoading from 'dva-loading'
-import createHistory from 'history/createBrowserHistory'
+import createHistory from 'history/createHashHistory'
 import './index.css'
 
 // 1. Initialize
